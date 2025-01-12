39cd5059c3c403b8fb7579c6c81bb64d7b5f1782
Moamal-2000 <moamalalaapro1@gmail.com>
2025-01-13 02:21:35 +0300

    add: initial boardArea data

---
+++ src/data/staticData.js
@@ -0,0 +1,74 @@
+export const boardArea = [
+  {
+    pieces: ["black", "black"],
+  },
+  {
+    pieces: [],
+  },
+  {
+    pieces: [],
+  },
+  {
+    pieces: [],
+  },
+  {
+    pieces: [],
+  },
+  {
+    pieces: ["white", "white", "white", "white", "white"],
+  },
+  {
+    pieces: [],
+  },
+  {
+    pieces: ["white", "white", "white"],
+  },
+  {
+    pieces: [],
+  },
+  {
+    pieces: [],
+  },
+  {
+    pieces: [],
+  },
+  {
+    pieces: ["black", "black", "black", "black", "black"],
+  },
+  {
+    pieces: ["white", "white", "white", "white", "white"],
+  },
+  {
+    pieces: [],
+  },
+  {
+    pieces: [],
+  },
+  {
+    pieces: [],
+  },
+  {
+    pieces: ["black", "black", "black"],
+  },
+  {
+    pieces: [],
+  },
+  {
+    pieces: ["black", "black", "black", "black", "black"],
+  },
+  {
+    pieces: [],
+  },
+  {
+    pieces: [],
+  },
+  {
+    pieces: [],
+  },
+  {
+    pieces: [],
+  },
+  {
+    pieces: ["white", "white"],
+  },
+];
