bcfb355d256fc734cdde6d206b11754f4be83498
Moamal-2000 <moamalalaapro1@gmail.com>
2025-01-13 03:13:50 +0300

    style: the places (triangles)

--- src/components/BackgammonBoard/BackgammonBoard.jsx
+++ src/components/BackgammonBoard/BackgammonBoard.jsx
@@ -5,8 +5,45 @@ import s from "./BackgammonBoard.module.scss";
 const BackgammonBoard = () => {
   return (
     <div className={s.board}>
-      <div className={s.leftTable}></div>
-      <div className={s.rightTable}></div>
+      <div className={s.leftTable}>
+        <div className={`${s.top} ${s.reverse}`}>
+          <div className={`${s.place} ${s.black}`} />
+          <div className={`${s.place} ${s.white}`} />
+          <div className={`${s.place} ${s.black}`} />
+          <div className={`${s.place} ${s.white}`} />
+          <div className={`${s.place} ${s.black}`} />
+          <div className={`${s.place} ${s.white}`} />
+        </div>
+
+        <div className={s.bottom}>
+          <div className={`${s.place} ${s.white}`} />
+          <div className={`${s.place} ${s.black}`} />
+          <div className={`${s.place} ${s.white}`} />
+          <div className={`${s.place} ${s.black}`} />
+          <div className={`${s.place} ${s.white}`} />
+          <div className={`${s.place} ${s.black}`} />
+        </div>
+      </div>
+
+      <div className={s.rightTable}>
+        <div className={`${s.top} ${s.reverse}`}>
+        <div className={`${s.place} ${s.black}`} />
+          <div className={`${s.place} ${s.white}`} />
+          <div className={`${s.place} ${s.black}`} />
+          <div className={`${s.place} ${s.white}`} />
+          <div className={`${s.place} ${s.black}`} />
+          <div className={`${s.place} ${s.white}`} />
+        </div>
+
+        <div className={s.bottom}>
+        <div className={`${s.place} ${s.white}`} />
+          <div className={`${s.place} ${s.black}`} />
+          <div className={`${s.place} ${s.white}`} />
+          <div className={`${s.place} ${s.black}`} />
+          <div className={`${s.place} ${s.white}`} />
+          <div className={`${s.place} ${s.black}`} />
+        </div>
+      </div>
     </div>
   );
 };
