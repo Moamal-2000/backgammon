e3d3405de6c80fbb84bc7bd7573a813722387d14
Moamal-2000 <moamalalaapro1@gmail.com>
2025-01-13 19:31:43 +0300

    feat: add unselect functionality for place selection

--- src/components/BackgammonBoard/PlacesWithPieces/PlacesWithPieces.jsx
+++ src/components/BackgammonBoard/PlacesWithPieces/PlacesWithPieces.jsx
@@ -6,15 +6,22 @@ import Pieces from "./Pieces/Pieces";
 import s from "./PlacesWithPieces.module.scss";
 
 const PlacesWithPieces = ({ placesData }) => {
-  const { playerTurn } = useSelector((state) => state.game);
+  const { playerTurn, selectPlace } = useSelector((state) => state.game);
   const dispatch = useDispatch();
 
   function handlePlaceClick(event, data, type) {
     const placeHasPieces = data.pieces.length;
     const isPlayerPiece = playerTurn === data.pieces?.[0];
+    const unSelectPlace = data.place === selectPlace;
 
-    if (placeHasPieces && isPlayerPiece)
+    if (unSelectPlace) {
+      dispatch(updateGameState({ key: "selectPlace", value: null }));
+      return;
+    }
+
+    if (placeHasPieces && isPlayerPiece) {
       dispatch(updateGameState({ key: "selectPlace", value: data.place }));
+    }
   }
 
   return placesData.map((data) => (
