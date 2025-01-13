ce1059e7569bedb0fab6fa499f05002a819223d3
Moamal-2000 <moamalalaapro1@gmail.com>
2025-01-13 03:43:50 +0300

    style: game pieces & change board colors

--- src/components/BackgammonBoard/BackgammonBoard.jsx
+++ src/components/BackgammonBoard/BackgammonBoard.jsx
@@ -7,41 +7,88 @@ const BackgammonBoard = () => {
     <div className={s.board}>
       <div className={s.leftTable}>
         <div className={`${s.top} ${s.reverse}`}>
-          <div className={`${s.place} ${s.black}`} />
-          <div className={`${s.place} ${s.white}`} />
-          <div className={`${s.place} ${s.black}`} />
-          <div className={`${s.place} ${s.white}`} />
-          <div className={`${s.place} ${s.black}`} />
-          <div className={`${s.place} ${s.white}`} />
+          <div className={`${s.place} ${s.black}`}>
+            <div className={`${s.piece} ${s.white}`} />
+            <div className={`${s.piece} ${s.white}`} />
+            <div className={`${s.piece} ${s.white}`} />
+            <div className={`${s.piece} ${s.white}`} />
+            <div className={`${s.piece} ${s.white}`} />
+          </div>
+
+          <div className={`${s.place} ${s.white}`}></div>
+          <div className={`${s.place} ${s.black}`}></div>
+          <div className={`${s.place} ${s.white}`}></div>
+
+          <div className={`${s.place} ${s.black}`}>
+            <div className={`${s.piece} ${s.black}`} />
+            <div className={`${s.piece} ${s.black}`} />
+            <div className={`${s.piece} ${s.black}`} />
+          </div>
+
+          <div className={`${s.place} ${s.white}`}></div>
         </div>
 
         <div className={s.bottom}>
-          <div className={`${s.place} ${s.white}`} />
-          <div className={`${s.place} ${s.black}`} />
-          <div className={`${s.place} ${s.white}`} />
-          <div className={`${s.place} ${s.black}`} />
-          <div className={`${s.place} ${s.white}`} />
-          <div className={`${s.place} ${s.black}`} />
+          <div className={`${s.place} ${s.white}`}>
+            <div className={`${s.piece} ${s.black}`} />
+            <div className={`${s.piece} ${s.black}`} />
+            <div className={`${s.piece} ${s.black}`} />
+            <div className={`${s.piece} ${s.black}`} />
+            <div className={`${s.piece} ${s.black}`} />
+          </div>
+          <div className={`${s.place} ${s.black}`}></div>
+          <div className={`${s.place} ${s.white}`}></div>
+          <div className={`${s.place} ${s.black}`}></div>
+
+          <div className={`${s.place} ${s.white}`}>
+            <div className={`${s.piece} ${s.white}`} />
+            <div className={`${s.piece} ${s.white}`} />
+            <div className={`${s.piece} ${s.white}`} />
+          </div>
+
+          <div className={`${s.place} ${s.black}`}></div>
         </div>
       </div>
 
       <div className={s.rightTable}>
         <div className={`${s.top} ${s.reverse}`}>
-        <div className={`${s.place} ${s.black}`} />
-          <div className={`${s.place} ${s.white}`} />
-          <div className={`${s.place} ${s.black}`} />
-          <div className={`${s.place} ${s.white}`} />
-          <div className={`${s.place} ${s.black}`} />
-          <div className={`${s.place} ${s.white}`} />
+          <div className={`${s.place} ${s.black}`}>
+            <div className={`${s.piece} ${s.black}`} />
+            <div className={`${s.piece} ${s.black}`} />
+            <div className={`${s.piece} ${s.black}`} />
+            <div className={`${s.piece} ${s.black}`} />
+            <div className={`${s.piece} ${s.black}`} />
+          </div>
+
+          <div className={`${s.place} ${s.white}`}></div>
+          <div className={`${s.place} ${s.black}`}></div>
+          <div className={`${s.place} ${s.white}`}></div>
+          <div className={`${s.place} ${s.black}`}></div>
+
+          <div className={`${s.place} ${s.white}`}>
+            <div className={`${s.piece} ${s.white}`} />
+            <div className={`${s.piece} ${s.white}`} />
+          </div>
         </div>
 
         <div className={s.bottom}>
-        <div className={`${s.place} ${s.white}`} />
-          <div className={`${s.place} ${s.black}`} />
-          <div className={`${s.place} ${s.white}`} />
-          <div className={`${s.place} ${s.black}`} />
-          <div className={`${s.place} ${s.white}`} />
-          <div className={`${s.place} ${s.black}`} />
+          <div className={`${s.place} ${s.white}`}>
+            <div className={`${s.piece} ${s.white}`} />
+            <div className={`${s.piece} ${s.white}`} />
+            <div className={`${s.piece} ${s.white}`} />
+            <div className={`${s.piece} ${s.white}`} />
+            <div className={`${s.piece} ${s.white}`} />
+          </div>
+
+          <div className={`${s.place} ${s.black}`}></div>
+          <div className={`${s.place} ${s.white}`}></div>
+          <div className={`${s.place} ${s.black}`}></div>
+          <div className={`${s.place} ${s.white}`}></div>
+
+          <div className={`${s.place} ${s.black}`}>
+            <div className={`${s.piece} ${s.black}`} />
+            <div className={`${s.piece} ${s.black}`} />
+          </div>
         </div>
       </div>
     </div>
