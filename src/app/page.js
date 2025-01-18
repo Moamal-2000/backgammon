f82da44d20fefe5f9a3dd51cf987d86257fe605f
Moamal-2000 <moamalalaapro1@gmail.com>
2025-01-18 06:17:19 +0300

    feat: player turn logic & basic styling for TurnTracker

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,6 @@ export default function Home() {
   return (
     <main className={s.home}>
       <div className="container" data-container>
-        <h1>Backgammon game!</h1>
 
         <div className={s.backgammonWrapper}>
           <TurnTracker />
--- src/components/BackgammonBoard/TurnTracker/TurnTracker.jsx
+++ src/components/BackgammonBoard/TurnTracker/TurnTracker.jsx
@@ -1,7 +1,26 @@
+"use client";
+
+import { useSelector } from "react-redux";
 import s from "./TurnTracker.module.scss";
 
 const TurnTracker = () => {
-  return <div className={s.turnTracker}></div>;
+  const { playerTurn } = useSelector((s) => s.game);
+  const whiteActiveClass = playerTurn === "white" ? s.white : "";
+  const blackActiveClass = playerTurn === "black" ? s.black : "";
+
+  return (
+    <div className={s.turnTracker}>
+      <div className={`${s.player} ${whiteActiveClass}`}>
+        {/* Icon */}
+        <span>P1</span>
+      </div>
+
+      <div className={`${s.player} ${blackActiveClass}`}>
+        {/* Icon */}
+        <span>P2</span>
+      </div>
+    </div>
+  );
 };
 
 export default TurnTracker;
