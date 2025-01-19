9443bd1c0d45841d818b187ad1df9e1ddafdfe04
Moamal-2000 <moamalalaapro1@gmail.com>
2025-01-19 07:12:16 +0300

    style: highlight active navigation links

--- src/Components/Navigator/Navigator.jsx
+++ src/Components/Navigator/Navigator.jsx
@@ -2,18 +2,24 @@
 
 import { NAV_LINKS } from "@/Data/staticData";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import SvgIcon from "../Shared/SvgIcon";
 import s from "./Navigator.module.scss";
 
 const Navigator = () => {
   return (
     <nav className={s.nav}>
-      {NAV_LINKS.map(({ id, name, path, iconName }) => (
-        <Link href={path} key={id}>
-          <SvgIcon name={iconName} />
-          <span>{name}</span>
-        </Link>
-      ))}
+      {NAV_LINKS.map(({ id, name, path, iconName }) => {
+        const currentPath = usePathname();
+        const activeLink = path === currentPath ? s.active : "";
+
+        return (
+          <Link href={path} key={id} className={activeLink}>
+            <SvgIcon name={iconName} />
+            <span>{name}</span>
+          </Link>
+        );
+      })}
     </nav>
   );
 };
