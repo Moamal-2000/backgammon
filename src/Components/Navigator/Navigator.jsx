6b347c1761a68e084f32dd01c7085d28953ce253
Moamal-2000 <moamalalaapro1@gmail.com>
2025-01-19 06:48:35 +0300

    feat: render nav links

--- src/Components/Navigator/Navigator.jsx
+++ src/Components/Navigator/Navigator.jsx
@@ -1,7 +1,19 @@
+"use client";
+
+import { NAV_LINKS } from "@/Data/staticData";
+import Link from "next/link";
 import s from "./Navigator.module.scss";
 
 const Navigator = () => {
-  return <nav className={s.nav}></nav>;
+  return (
+    <nav className={s.nav}>
+      {NAV_LINKS.map(({ id, name, path }) => (
+        <Link href={path} key={id}>
+          {name}
+        </Link>
+      ))}
+    </nav>
+  );
 };
 
 export default Navigator;
--- src/Data/staticData.js
+++ src/Data/staticData.js
@@ -1,3 +1,26 @@
 import { getBoardArea } from "@/Functions/helper";
 
 export const boardArea = getBoardArea();
+
+export const NAV_LINKS = [
+  {
+    name: "Basics",
+    path: "/",
+    id: 1,
+  },
+  {
+    name: "Rules",
+    path: "/rules",
+    id: 2,
+  },
+  {
+    name: "Strategy",
+    path: "/strategy",
+    id: 3,
+  },
+  {
+    name: "Setup",
+    path: "/setup",
+    id: 4,
+  },
+];
