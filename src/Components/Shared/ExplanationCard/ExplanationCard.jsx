c6b5a6b92993ebeed20f64e7d93c5251df34f88a
Moamal-2000 <moamalalaapro1@gmail.com>
2025-01-20 00:25:16 +0300

    feat: make ExplanationCard component render dynamic data by using props

--- src/Components/Shared/ExplanationCard/ExplanationCard.jsx
+++ src/Components/Shared/ExplanationCard/ExplanationCard.jsx
@@ -1,7 +1,20 @@
 import s from "./ExplanationCard.module.scss";
 
-const ExplanationCard = () => {
-  return <div className={s.card}></div>;
+const ExplanationCard = ({ mainTitle, explanationData }) => {
+  return (
+    <div className={s.mainCard}>
+      <h2>{mainTitle}</h2>
+
+      {explanationData?.map(({ title, descriptions }) => (
+        <div className={s.card}>
+          <h3>{title}</h3>
+          {descriptions?.map((description) => (
+            <p>{description}</p>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default ExplanationCard;
