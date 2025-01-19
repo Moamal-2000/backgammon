78ae60a59c42f46aecd1c959db26b812dc47df11
Moamal-2000 <moamalalaapro1@gmail.com>
2025-01-19 06:31:28 +0300

    style: header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,7 +3,17 @@
 import s from "./Header.module.scss";
 
 const Header = () => {
-  return <header className={s.header}></header>;
+  return (
+    <header className={s.header}>
+      <h1>Backgammon</h1>
+
+      <div className={s.shape}>
+        <div className={s.line} />
+      </div>
+
+      <p>Master one of the oldest and most strategic board games in history</p>
+    </header>
+  );
 };
 
 export default Header;
