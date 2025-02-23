e571daa14648d40c7b906173fdf3cd7dc3787450
Moamal-2000 <moamalalaapro1@gmail.com>
2025-02-23 03:07:01 +0300

    feat: stacked pieces levels

--- src/Components/Pages/Home/BackgammonBoard/Places/Pieces/Piece/Piece.jsx
+++ src/Components/Pages/Home/BackgammonBoard/Places/Pieces/Piece/Piece.jsx
@@ -3,8 +3,9 @@
 import u from "@/Styles/utilsClasses.module.scss";
 import { useSelector } from "react-redux";
 
-const Piece = ({ data, piece, isLastPiece, unavailableClass }) => {
+const Piece = ({ data, piece, isLastPiece, unavailableClass, pieceOrder }) => {
   const { selectedPlace } = useSelector((s) => s.game);
+
   const selectClass = data.place === selectedPlace ? u.select : "";
   const classes = `${u.piece} ${u[piece]} ${unavailableClass} ${
     isLastPiece ? selectClass : ""
--- src/Components/Pages/Home/BackgammonBoard/Places/Pieces/Pieces.jsx
+++ src/Components/Pages/Home/BackgammonBoard/Places/Pieces/Pieces.jsx
@@ -1,17 +1,91 @@
 "use client";
 
 import Piece from "./Piece/Piece";
+import s from "./Pieces.module.scss";
 
 const Pieces = ({ data, unavailableClass }) => {
-  return data.pieces.map((piece, index) => (
-    <Piece
-      key={index}
-      data={data}
-      piece={piece}
-      isLastPiece={0 === index}
-      unavailableClass={unavailableClass}
-    />
-  ));
+  const baseStack = data.pieces.slice(0, 5);
+  const secondStack = data.pieces.slice(5, 9);
+  const thirdStack = data.pieces.slice(9, 12);
+  const fourthStack = data.pieces.slice(12, 14);
+  const lastStack = data.pieces.slice(14, 15);
+
+  return (
+    <div className={s.baseStack}>
+      {baseStack.map((piece, index) => {
+        return (
+          <Piece
+            key={index}
+            data={data}
+            piece={piece}
+            isLastPiece={0 === index}
+            pieceOrder={index + 1}
+            unavailableClass={unavailableClass}
+          />
+        );
+      })}
+
+      <div className={s.secondStack}>
+        {secondStack.map((piece, index) => {
+          return (
+            <Piece
+              key={index}
+              data={data}
+              piece={piece}
+              isLastPiece={0 === index}
+              pieceOrder={index + 1}
+              unavailableClass={unavailableClass}
+            />
+          );
+        })}
+
+        <div className={s.thirdStack}>
+          {thirdStack.map((piece, index) => {
+            return (
+              <Piece
+                key={index}
+                data={data}
+                piece={piece}
+                isLastPiece={0 === index}
+                pieceOrder={index + 1}
+                unavailableClass={unavailableClass}
+              />
+            );
+          })}
+
+          <div className={s.fourthStack}>
+            {fourthStack.map((piece, index) => {
+              return (
+                <Piece
+                  key={index}
+                  data={data}
+                  piece={piece}
+                  isLastPiece={0 === index}
+                  pieceOrder={index + 1}
+                  unavailableClass={unavailableClass}
+                />
+              );
+            })}
+
+            <div className={s.lastStack}>
+              {lastStack.map((piece, index) => {
+                return (
+                  <Piece
+                    key={index}
+                    data={data}
+                    piece={piece}
+                    isLastPiece={0 === index}
+                    pieceOrder={index + 1}
+                    unavailableClass={unavailableClass}
+                  />
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Pieces;
