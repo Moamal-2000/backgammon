b033015a5a00bcdd9efcdcfa125c676ce59936b9
Moamal-2000 <moamalalaapro1@gmail.com>
2025-02-25 10:16:22 +0300

    refactor: streamline Pieces component rendering using reduceRight for stack mapping

--- src/Components/Pages/Home/BackgammonBoard/Places/Pieces/Pieces.jsx
+++ src/Components/Pages/Home/BackgammonBoard/Places/Pieces/Pieces.jsx
@@ -6,89 +6,25 @@ import s from "./Pieces.module.scss";
 
 const Pieces = ({ data, unavailableClass }) => {
   const { stacks, lastStackName } = getStackData(data.pieces);
-  const { baseStack, secondStack, thirdStack, fourthStack, lastStack } = stacks;
+  const stackEntries = Object.entries(stacks);
 
-  return (
-    <div className={s.baseStack}>
-      {baseStack.map((piece, index) => {
-        return (
-          <Piece
-            key={index}
-            data={data}
-            piece={piece}
-            isLastPiece={0 === index}
-            unavailableClass={unavailableClass}
-            lastStackName={lastStackName}
-            stackName="baseStack"
-          />
-        );
-      })}
+  return stackEntries.reduceRight((child, [stackName, stack]) => (
+    <div key={stackName} className={s[stackName]}>
+      {stack.map((piece, index) => (
+        <Piece
+          key={index}
+          data={data}
+          piece={piece}
+          isLastPiece={index === 0}
+          unavailableClass={unavailableClass}
+          lastStackName={lastStackName}
+          stackName={stackName}
+        />
+      ))}
 
-      <div className={s.secondStack}>
-        {secondStack.map((piece, index) => {
-          return (
-            <Piece
-              key={index}
-              data={data}
-              piece={piece}
-              isLastPiece={0 === index}
-              unavailableClass={unavailableClass}
-              lastStackName={lastStackName}
-              stackName="secondStack"
-            />
-          );
-        })}
-
-        <div className={s.thirdStack}>
-          {thirdStack.map((piece, index) => {
-            return (
-              <Piece
-                key={index}
-                data={data}
-                piece={piece}
-                isLastPiece={0 === index}
-                unavailableClass={unavailableClass}
-                lastStackName={lastStackName}
-                stackName="thirdStack"
-              />
-            );
-          })}
-
-          <div className={s.fourthStack}>
-            {fourthStack.map((piece, index) => {
-              return (
-                <Piece
-                  key={index}
-                  data={data}
-                  piece={piece}
-                  isLastPiece={0 === index}
-                  unavailableClass={unavailableClass}
-                  lastStackName={lastStackName}
-                  stackName="fourthStack"
-                />
-              );
-            })}
-
-            <div className={s.lastStack}>
-              {lastStack.map((piece, index) => {
-                return (
-                  <Piece
-                    key={index}
-                    data={data}
-                    piece={piece}
-                    isLastPiece={0 === index}
-                    unavailableClass={unavailableClass}
-                    lastStackName={lastStackName}
-                    stackName="lastStack"
-                  />
-                );
-              })}
-            </div>
-          </div>
-        </div>
-      </div>
+      {child}
     </div>
-  );
+  ));
 };
 
 export default Pieces;
