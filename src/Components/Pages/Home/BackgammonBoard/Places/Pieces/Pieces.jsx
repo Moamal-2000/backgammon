a39bdd29096e959ffe5a47bd2fd0c916cf4aa40a
Moamal-2000 <moamalalaapro1@gmail.com>
2025-02-25 10:28:24 +0300

    fix [stack pieces]: return null if there is no pieces in the place

--- src/Components/Pages/Home/BackgammonBoard/Places/Pieces/Pieces.jsx
+++ src/Components/Pages/Home/BackgammonBoard/Places/Pieces/Pieces.jsx
@@ -8,23 +8,26 @@ const Pieces = ({ data, unavailableClass }) => {
   const { stacks, lastStackName } = getStackData(data.pieces);
   const stackEntries = Object.entries(stacks);
 
-  return stackEntries.reduceRight((child, [stackName, stack]) => (
-    <div key={stackName} className={s[stackName]}>
-      {stack.map((piece, index) => (
-        <Piece
-          key={index}
-          data={data}
-          piece={piece}
-          isLastPiece={index === 0}
-          unavailableClass={unavailableClass}
-          lastStackName={lastStackName}
-          stackName={stackName}
-        />
-      ))}
+  return stackEntries.reduceRight(
+    (child, [stackName, stack]) => (
+      <div key={stackName} className={s[stackName]}>
+        {stack.map((piece, index) => (
+          <Piece
+            key={index}
+            data={data}
+            piece={piece}
+            isLastPiece={index === 0}
+            unavailableClass={unavailableClass}
+            lastStackName={lastStackName}
+            stackName={stackName}
+          />
+        ))}
 
-      {child}
-    </div>
-  ));
+        {child}
+      </div>
+    ),
+    null
+  );
 };
 
 export default Pieces;
