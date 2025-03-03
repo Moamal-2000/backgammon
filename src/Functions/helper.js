b744d553047c336ee8215d299f40e23b7d7bebd7
Moamal-2000 <moamalalaapro1@gmail.com>
2025-03-03 19:32:55 +0300

    write: getExpandedDiceMoves function

--- src/Functions/helper.js
+++ src/Functions/helper.js
@@ -277,6 +277,21 @@ export function calcAvailablePlaces({ boardArea, diceMoves, playerTurn }) {
   return updatedBoardArea;
 }
 
+export function getExpandedDiceMoves(diceMoves) {
+  const result = [...diceMoves];
+
+  for (let i = 0; i < diceMoves.length; i++) {
+    let sum = diceMoves[i];
+
+    for (let j = i + 1; j < diceMoves.length; j++) {
+      sum += diceMoves[j];
+      result.push(sum);
+    }
+  }
+
+  return [...new Set(result)];
+}
+
 export function getAvailableMove({
   diceMove,
   placeData,
